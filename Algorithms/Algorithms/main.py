988ccef3b6147df8a403e5b55699e849cb0990bc
aajawad <aajawad1997@gmail.com>
2023-05-01 18:36:14 +0100

    updated readme

--- Algorithms/Algorithms/main.py
+++ Algorithms/Algorithms/main.py
@@ -7,16 +7,11 @@ from Floyd_Recursive import floyd
 INF = float('inf')
 
 #Define the graph to be called (represented as a matrix)
-graph = [[0, 16, INF, 71, INF, 94, 30, INF, INF],
-          [INF, 0, 43, INF, INF, INF, INF, INF, INF],
-          [INF, INF, 0, INF, INF, INF, INF, INF, INF],
-          [INF, INF, INF, 0, INF, 5, 75, INF, INF],
-          [INF, INF, INF, INF, 0, INF, 66, 52, INF],
-          [INF, INF, INF, INF, INF, 0, 29, INF, 48],
-          [INF, INF, INF, INF, INF, INF, 0, 84, INF],
-          [INF, INF, INF, INF, INF, INF, INF, 0, INF],
-          [INF, INF, INF, INF, INF, INF, INF, INF, 0]
-         ]
+graph = [[0, 5, INF, 10],
+         [INF, 0, 3, INF],
+         [INF, INF, 0, 1],
+         [INF, INF, INF, 0]
+        ]
 
 floyd(graph)
 
--- README.md
+++ README.md
@@ -6,3 +6,23 @@ This is a directory that contains a recurisve function of the Floyd-Warshall alg
 
 In main.py, enter the graph that you wish to test, an example is below:
 
+```
+#Main Script to call the recursive Floyd Warshall Algorithm
+
+from Floyd_Recursive import floyd
+
+
+#Define infinity. The value is used for vertices not connected to each other
+INF = float('inf')
+
+#Define the graph to be called (represented as a matrix)
+graph = [[0, 5, INF, 10],
+         [INF, 0, 3, INF],
+         [INF, INF, 0, 1],
+         [INF, INF, INF, 0]
+        ]
+
+floyd(graph)
+```
+
+
